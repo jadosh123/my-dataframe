46a5d7d700b4b3828f108a64c63d795d61e53dc5
jad <jadosh2000@gmail.com>
2025-10-17 21:49:43 +0300

    First version of the type checker its probably bad

--- utils/type_check.py
+++ utils/type_check.py
@@ -1,4 +1,4 @@
-import numpy as np
+from collections.abc import Mapping
 
 
 def type_checker(data: list):
@@ -6,7 +6,23 @@ def type_checker(data: list):
     A type checking function for the series, it returns the datatype.
     """
     # Most general type as placeholder until we determine the type
-    safest_type = object
-    
+    data_types = set()
+
     for element in data:
-        if type(element)
+        data_types.add(type(element))
+
+    for dtype in data_types:
+        if dict in data_types:
+            return dict
+        elif str in data_types:
+            return str
+        elif float in data_types:
+            return float
+        else:
+            return int
+
+
+if __name__ == "__main__":
+    tmp = [1, 'hello', 3, 4.5, dict(tmp=1)]
+    dtype = type_checker(tmp)
+    print(dtype)
